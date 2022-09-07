bfc81f99758efc384a8dc2e8f9f2933d16e3adf6
Ruslan Sabrekov <rsabrekov@theentrustgroup.com>
2022-09-07 11:32:39 +0300

    upgrade artifact version to 0.2.3

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@
 .\prunsrv.exe //IS//CUSTOMHWINFO --DisplayName="AACustom HWInfo" --Description="Process data from COM port and write it to registry" ^
 --Install="%~dp0prunsrv.exe" --Jvm="C:\Program Files\Zulu\zulu-17\bin\server\jvm.dll" ^
 ++JvmOptions=-Dservice.config="%~dp0service.properties" ^
---Classpath="%~dp0TestService-0.2-jar-with-dependencies.jar" --LogLevel=DEBUG ^
+--Classpath="%~dp0TestService-0.2.3-jar-with-dependencies.jar" --LogLevel=DEBUG ^
 --LogPath=d:\pets --LogPrefix=testService.log --Startup=manual --StdOutput=%~dp0stdout.log ^
 --StdError=%~dp0stderr.log --JvmMs=1 --JvmMx=10 --StartMode=jvm --StartClass=org.example.ServiceLauncher ^
 --StartMethod=windowsService ++StartParams="start" --StopMode=jvm --StopClass=org.example.ServiceLauncher ^
